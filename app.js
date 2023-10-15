caba4f8d2212de95828f0c06f9c06e68f30399a6
kkebuka <kkebuka@gmai.com>
2023-10-15 23:44:42 +0100

    update on Js and Htm files

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
 const image1 = document.querySelector(".img1");
 const image2 = document.querySelector(".img2");
-const winner = document.querySelector("h1")
+const winner = document.querySelector("h1");
+const playGame = document.querySelector(".playGame")
 
-document.addEventListener("DOMContentLoaded", function()
+// let n = Math.random();
+// let m = Math.random();
+// let die1;
+// let die2;
+// die1 = Math.floor((n * 6)) + 1;
+// die2 = Math.round((m *6)) + 1 ;
+// console.log(die1);
+// console.log(die2);
+// console.log(playGame);
+
+
+playGame.addEventListener("click", function(e)
  {
-    let n = Math.random();
-    let m = Math.random();
-    let die1;
-    let die2 = m * m;
-    die1 = Math.floor((n * 6) + 1);
-    die2 = Math.round((die2 * 5) + 1 );
+ let n = Math.random();
+let m = Math.random();
+let die1;
+let die2;
+die1 = Math.floor((n * 6)) + 1;
+die2 = Math.round((m *6)) + 1 ;
+console.log(die1);
+console.log(die2);
+console.log(playGame);
 
+    diceResult(die1,image1);
 
-    if(die1 === 1){
-        image1.src="/images/dice1.png"
-    }else if(die1 === 2){
-        image1.src="/images/dice2.png"
-    }else if(die1 === 3){
-        image1.src="/images/dice3.png"
-    }else if(die1 === 4){
-        image1.src="/images/dice4.png"
-    }else if(die1 === 5){
-        image1.src="/images/dice5.png"
-    }else if(die1 === 6){
-        image1.src="/images/dice6.png"
-   }
-
-    if(die2 === 1){
-        image2.src="/images/dice1.png"
-    }else if(die2 === 2){
-        image2.src="/images/dice2.png"
-    }else if(die2 === 3){
-        image2.src="/images/dice3.png"
-    }else if(die2 === 4){
-        image2.src="/images/dice4.png"
-    }else if(die2 === 5){
-        image2.src="/images/dice5.png"
-    }else if(die2 === 6){
-    image2.src="/images/dice6.png"
-   }
-
+    diceResult(die2,image2);
+    
    if(die1 > die2){
     winner.innerHTML="Player 1 wins"
    } else if(die1 === die2){
@@ -47,6 +37,28 @@ document.addEventListener("DOMContentLoaded", function()
    } else if(die2 > die1){
     winner.innerHTML="Player 2 wins"
    }
+   e.preventDefault();
 });
 
+
+function diceResult(type,imageSource){
+    let roll;
+    let image;
+        roll = type;
+        image = imageSource;
+    
+    if(roll === 1){
+        image.src="/images/dice1.png"
+    }else if(roll === 2){
+        image.src="/images/dice2.png"
+    }else if(roll === 3){
+        image.src="/images/dice3.png"
+    }else if(roll === 4){
+        image.src="/images/dice4.png"
+    }else if(roll === 5){
+        image.src="/images/dice5.png"
+    }else if(roll === 6){
+    image.src="/images/dice6.png"
+       }
+    };
 
